a3a2d68153ebeb0ca57d0897731c74a83f7ce5f1
itseungyeon <geniusy92@gmail.com>
2025-08-07 01:53:44 +0900

    깃페이지설정 확인테스트

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="bg-blue-500 p-10 text-center">
-      <h1 className="text-6xl font-extrabold text-white">🎂시호의 첫번째 생일파티에 초대합니다🎂</h1>
+      <h1 className="text-6xl font-extrabold text-white">🎂시호의 첫번째 생일파티에 초대합니다!!!!!!🎂</h1>
     </div>
   );
 }
